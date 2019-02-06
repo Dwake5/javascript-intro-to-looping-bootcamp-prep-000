bcca19ba901cb8c380df57dd4449cf7bb4944026
Danny Wakeling <wakeling101@hotmail.com>
2019-02-06 11:35:21 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    if (i===1){
+      array.push('I am 1 strange loop.')
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
+}
+
+function whileLoop(no){
+  while(no > 0){
+    console.log(--no)
+  } if (no === 0){
+    return('done')
+  }
+}
+
+function doWhileLoop(int){
+  let numb = 0
+  function incrementVariable(){
+  numb = numb++
+  return numb;
+  }
+  do {
+    console.log('I run once regardless.')
+    incrementVariable()
+  } while (incrementVariable() < int)
+}
